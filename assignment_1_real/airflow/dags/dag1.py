9945a432227f1d340fbd5e251af2f1f3a177366d
Sukruth Mothakapally <sukruth.mothakapally@gmail.com>
2023-06-11 21:16:51 -0700

    dockerized streamlit and other updates

--- assignment_1_real/airflow/dags/dag1.py
+++ assignment_1_real/airflow/dags/dag1.py
@@ -8,16 +8,16 @@ from airflow.operators.docker_operator import DockerOperator
 
 # dag declaration
 user_input = {
-    "dataset_name": Param(default="20160219_CIR", type='string', minLength=5, maxLength=255),
+    "dataset_name": Param(default="20150721_AAPL", type='string', minLength=5, maxLength=255),
 }
 
 dag = DAG(
-    dag_id="demo_data_load",
+    dag_id="Data_Pipeline",
     schedule="0 0 * * *",   # https://crontab.guru/
     start_date=days_ago(0),
     catchup=False,
     dagrun_timeout=timedelta(minutes=60),
-    tags=["labs", "damg7245"],
+    tags=["Assignment-1", "damg7245"],
     # default_args=args,
     params=user_input,
 )
@@ -26,12 +26,12 @@ def get_data_from_github(**kwargs):
     import requests
     import csv
     from collections import defaultdict
+    import numpy as np
+
     # Get data from GitHub
     url = f"https://raw.githubusercontent.com/Earnings-Call-Dataset/MAEC-A-Multimodal-Aligned-Earnings-Conference-Call-Dataset-for-Financial-Risk-Prediction/master/MAEC_Dataset/{kwargs['params']['dataset_name']}/text.txt"
     page = requests.get(url)
-    words = page.text.split()
-    first_500_words = ' '.join(words[:250])
-    #print(first_500_words)
+    transcript = page.text
 
     # Get ticker and company name
     tickers_url = "https://raw.githubusercontent.com/plotly/dash-stock-tickers-demo-app/master/tickers.csv"
@@ -39,35 +39,76 @@ def get_data_from_github(**kwargs):
     tickers_data = defaultdict(str)
     for row in csv.DictReader(tickers_page.text.splitlines()):
         tickers_data[row['Symbol']] = row['Company']
-    
-    response = {}
 
-    response["date"] = kwargs['params']['dataset_name'].split('_')[0]
-    response["plain_text"] = first_500_words
-    response["ticker"] = kwargs['params']['dataset_name'].split('_')[-1]
-    response["company_name"] = tickers_data[response["ticker"]]
+    # SBert Embeddings
+    from sentence_transformers import SentenceTransformer
+    sbert_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+    import openai
+    openai.api_key = "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"
+
+    def get_openai_embeddings(prompt):
+        response = openai.Embedding.create(
+            input=prompt,
+            model="text-embedding-ada-002"
+        )
+        return response['data'][0]['embedding']
+
+    words = transcript.split()
+    chunks = [words[i:i + 500] for i in range(0, len(words), 500)]
+
+    response = []
+
+    for i, chunk in enumerate(chunks):
+        response_chunk = {}
+
+        response_chunk["date"] = kwargs['params']['dataset_name'].split('_')[0]
+        response_chunk["plain_text"] = ' '.join(chunk)
+        response_chunk["ticker"] = kwargs['params']['dataset_name'].split('_')[-1]
+        response_chunk["company_name"] = tickers_data.get(response_chunk["ticker"], "Company not found")
+        response_chunk["Part"] = i + 1
+
+        # Encode chunk with SBert model
+        sbert_embeddings = sbert_model.encode([' '.join(chunk)], convert_to_tensor=True).numpy()
+        response_chunk["sbert_embeddings"] = sbert_embeddings.tostring()
+
+        # Encode chunk with OpenAI model
+        openai_embeddings = get_openai_embeddings(' '.join(chunk))
+        openai_embeddings = np.array(openai_embeddings)
+        response_chunk["openai_embeddings"] = openai_embeddings.tostring()
+
+        response.append(response_chunk)
+
+    # Store data in Redis
+    import os
+    import redis
+
+    r = redis.Redis(
+        host=os.getenv("DB_HOST", "redis-stack"),
+        port=os.getenv("DB_PORT", "6379"),
+        username=os.getenv("DB_USERNAME", ""),
+        password=os.getenv("DB_PASSWORD", ""),
+        decode_responses=True
+    )
+
+    if r.ping():
+        for item in response:
+            hash_key = f"{item['company_name']}:{item['date']}:{item['Part']}"
+            #item["sbert_embeddings"] = item["sbert_embeddings"].tostring()
+            #item["openai_embeddings"] = item["openai_embeddings"].tostring()
+            r.hset(hash_key, mapping=item)
+
+        r.close()
+        print("Data loaded to Redis")
+    else:
+        print("Failed to connect to Redis")
 
     print(response)
 
-    #redis db to store data of entered company (date, plaintext, ticker, company name) and uses hash of- company name,date columns    
-    import os
-    import redis
-    r = redis.Redis(host=os.getenv("DB_HOST", "redis-stack"),  # Local redis error
-                    port=os.getenv("DB_PORT", "6379"),
-                    username=os.getenv("DB_USERNAME", ""),
-                    password=os.getenv("DB_PASSWORD", ""),
-                    decode_responses=True
-                    )
-
-    print(r.ping())
-    r.hset(f"{kwargs['params']['dataset_name'].split('_')[-1]}:{kwargs['params']['dataset_name'].split('_')[0]}", mapping=response)
-    r.close()
-    print("Data loaded to redis")
-
-with dag:   
+with dag:  
 
     get_data_from_github = PythonOperator(
-        task_id='get_data_from_github',
+        task_id='github_redis',
         python_callable=get_data_from_github,
         provide_context=True,
         dag=dag,
--- assignment_1_real/streamlit/pages/part-1.py
+++ assignment_1_real/streamlit/pages/part-1.py
@@ -1,41 +1,59 @@
 import os
 import redis
 import streamlit as st
+import numpy as np
+from datetime import datetime
 
 st.title('Company Data')
 
-redis_conn = redis.Redis(host="localhost",
-                         port=30001,
+redis_conn = redis.Redis(host="redis-stack",
+                         port=6379,
                          username=os.getenv("DB_USERNAME", ""),
                          password=os.getenv("DB_PASSWORD", ""),
-                         decode_responses=True
+                         decode_responses=False
                          )
 
 # Get list of companies and dates from Redis
 companies = []
 dates = []
-for key in redis_conn.keys("*:*"):
-    ticker, date = key.split(":")
-    companies.append(ticker)
+for key in redis_conn.keys("*:*:1"):
+    key_str = key.decode('utf-8')
+    company_name, date, part = key_str.split(":")
+    companies.append(company_name)
     dates.append(date)
 companies = sorted(list(set(companies)))
 dates = sorted(list(set(dates)))
+
+# Format dates
+dates = [datetime.strptime(date, '%Y%m%d').strftime('%b %d %Y') for date in dates]
 
 # User selection
 company = st.selectbox("Select a company", companies)
 date = st.selectbox("Select a date", dates)
 
 # Get data from Redis
-key = f"{company}:{date}"
+date_key = datetime.strptime(date, '%b %d %Y').strftime('%Y%m%d')
+key = f"{company}:{date_key}:1"
 data = redis_conn.hgetall(key)
 
 if data:
+    # Decode data
+    data = {k.decode('utf-8'): v for k, v in data.items()}
+
     # Display data
     st.write(f"Ticker: {data['ticker']}")
     st.write(f"Company Name: {data['company_name']}")
     st.write(f"Date: {data['date']}")
+    st.write(f"Part: {data['Part']}")
     st.write(f"Plain Text: {data['plain_text']}")
+
+    # Display embeddings
+    #to convert string to numpy array as redis stores data as string and we need to convert it back to numpy array
+    sbert_embeddings = np.frombuffer(data["sbert_embeddings"], dtype=np.float32)
+    openai_embeddings = np.frombuffer(data["openai_embeddings"], dtype=np.float32)
+    st.write(f"SBERT Embeddings: {sbert_embeddings}")
+    st.write(f"OpenAI Embeddings: {openai_embeddings}")
 
 else:
     #Display error message
-    st.write(f"Data not found for company: {company} on date: {date}") 
+    st.write(f"Data not found for company: {company} on date: {date}")
