9d4c3e1b04b2a5110436352f23e1f66e0c558c11
Sukruth Mothakapally <sukruth.mothakapally@gmail.com>
2023-06-13 08:18:05 -0700

    Implemented logic for openai and sbert VSS

--- assignment_1_real/streamlit/pages/part2.py
+++ assignment_1_real/streamlit/pages/part2.py
@@ -3,6 +3,9 @@ import redis
 import streamlit as st
 import numpy as np
 from datetime import datetime
+import openai
+import numpy as np
+from sentence_transformers import SentenceTransformer
 
 st.title('Part 2')
 
@@ -51,6 +54,37 @@ if "username" in st.session_state:
     key = f"{company}:{date_key}:{part}"
     data = redis_conn.hgetall(key)
 
+    class OpenAI:
+        def __init__(self, api_key):
+            self.api_key = api_key
+            openai.api_key = api_key
+
+        def embed_text(self, text, model="text-embedding-ada-002"):
+            text = text.replace("\n", " ")
+            return openai.Embedding.create(input=[text], model=model)['data'][0]['embedding']
+
+        def get_top_k_similar_sentences(self, original_sentences, user_input, k=5):
+            original_embeddings = [self.embed_text(sentence) for sentence in original_sentences]
+            user_input_embedding = self.embed_text(user_input)
+            similarities = [np.dot(user_input_embedding, original_embedding) for original_embedding in original_embeddings]
+            top_k_indices = np.argsort(similarities)[-k:]
+            return [original_sentences[i] for i in top_k_indices]
+        
+    from sentence_transformers import SentenceTransformer
+    sbert_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+    class SBERT:
+        def embed_text(self, text):
+            text = text.replace("\n", " ")
+            return sbert_model.encode([text])
+
+        def get_top_k_similar_sentences(self, original_sentences, user_input, k=5):
+            original_embeddings = [self.embed_text(sentence) for sentence in original_sentences]
+            user_input_embedding = self.embed_text(user_input)
+            similarities = [np.dot(user_input_embedding.reshape(-1), original_embedding.reshape(-1)) for original_embedding in original_embeddings]
+            top_k_indices = np.argsort(similarities)[-k:]
+            return [original_sentences[i] for i in top_k_indices]
+
     if data:
         # Decode data
         data = {k.decode('utf-8'): v for k, v in data.items()}
@@ -60,13 +94,24 @@ if "username" in st.session_state:
 
         # Display selected embedding
         if embedding_type == "OpenAI":
+            api_key = st.text_input("Enter your OpenAI API key")
             openai_embeddings = np.frombuffer(data["openai_embeddings"], dtype=np.float32)
             st.write(f"OpenAI Embeddings: {openai_embeddings}")
+            openai_helper = OpenAI(api_key)
+            user_input = st.text_input("Enter a sentence to compare with the embeddings")
+            if user_input:
+                top_5_similar_sentences = openai_helper.get_top_k_similar_sentences(data['plain_text'].decode('utf-8').split('. '), user_input)
+                for i, sentence in enumerate(top_5_similar_sentences):
+                    st.write(f"{i+1}. {sentence}")
         elif embedding_type == "SBERT":
             sbert_embeddings = np.frombuffer(data["sbert_embeddings"], dtype=np.float32)
             st.write(f"SBERT Embeddings: {sbert_embeddings}")
-        #textbox to enter a sentence
-        st.text_input("Enter a sentence to compare with the embeddings")
+            sbert_helper = SBERT()
+            user_input = st.text_input("Enter a sentence to compare with the embeddings")
+            if user_input:
+                top_5_similar_sentences = sbert_helper.get_top_k_similar_sentences(data['plain_text'].decode('utf-8').split('. '), user_input)
+                for i, sentence in enumerate(top_5_similar_sentences):
+                    st.write(f"{i+1}. {sentence}")
 
     else:
         # Display error message
--- welcome.py
+++
@@ -1,3 +0,0 @@
-print("Welcome to DAMG7245 - Big data systems and intelligence analytics")
-print("hello world")
-print("new branch")
