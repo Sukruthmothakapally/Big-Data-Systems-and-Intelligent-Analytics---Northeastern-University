e96fcfbdef1ab3bd802dd141413313e3cffd8b9d
Vedant-Deshpande <114270105+Vedant-Deshpande@users.noreply.github.com>
2023-08-05 12:58:17 -0400

    User sign up & User Dashboard

---
+++ final_project/streamlit/pages/UserDashboard.py
@@ -0,0 +1,11 @@
+# UserDashboard.py
+
+import streamlit as st
+
+def main():
+    st.title("User Dashboard")
+    st.write("Welcome to your dashboard!")
+    # Add your user dashboard content here
+
+if __name__ == "__main__":
+    main()
