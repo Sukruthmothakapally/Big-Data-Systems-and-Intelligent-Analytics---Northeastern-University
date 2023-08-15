62a055fd49276ed658581eb9a269600911ad79e6
Sukruth_Mothakapally <107236457+Sukruthmothakapally@users.noreply.github.com>
2023-08-14 19:31:28 -0700

    Update About.py

--- final_project/streamlit/About.py
+++ final_project/streamlit/About.py
@@ -18,5 +18,7 @@ st.markdown("""
 
 st.markdown("<h1 class='center'>Welcome to StackAI</h1>", unsafe_allow_html=True)
 
-st.markdown("<h2 class='green'>Features</h2>", unsafe_allow_html=True)
-st.markdown("<h3>Ask a question and STackAI will find the most similar topics using StackOverflow database</h3>", unsafe_allow_html=True)
+# st.markdown("<h2 class='green'>Features</h2>", unsafe_allow_html=True)
+st.markdown("<h3>Introducing StackAI – Your Ultimate Coding Companion! Wave goodbye to the challenge of hunting down solutions on Stack Overflow. StackAI revolutionizes your coding experience by instantly fetching the right answers written by humans, simplifying intricate topics, and even crafts personalized answers tailored to your unique questions..</h3>", unsafe_allow_html=True)
+st.markdown("<h3>Supercharge your coding journey now!</h3>", unsafe_allow_html=True)
+st.markdown("<h3>#UnlockTheCodeGenius 🚀</h3>", unsafe_allow_html=True)
