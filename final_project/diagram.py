08d4e6ff215dcc69f2c2d0a149cc7c453f9f333e
Vedant-Deshpande <114270105+Vedant-Deshpande@users.noreply.github.com>
2023-08-16 12:22:01 -0400

    Update diagram.py

--- final_project/diagram.py
+++ final_project/diagram.py
@@ -9,54 +9,53 @@ from diagrams.programming.language import Python
 from diagrams.onprem.workflow import Airflow
 from diagrams.onprem.compute import Server
 
-with Diagram("Proposed Architecture for STACKAI", show=True):
+with Diagram("Implemented Architecture for STACKAI", show=True):
 
     streamlit = Custom("Streamlit", "./logos/streamlit.jpg")
     docker = Docker("Docker")
     #github actions
     github = Custom("Github Actions", "./logos/githubactions.jpg")
     terraform = Terraform("Terraform")
-    gcp = GCE("Google Cloud Platform") 
+    gcp = Custom("App hosted on GCP","./logos/gcp.png") 
     fastapi = FastAPI("FAST API Backend")
-    snowflake = Snowflake("Snowflake")
-
+    bigquery = Custom("GCP Bigquery Data Source","./logos/gcpbigquery.png")
+    cloudsql = Custom("CloudSQL","./logos/cloudsql.png")
     airflow = Airflow("Airflow DAGs")
     
 
     with Cluster("Airflow"):
-        extract = Custom("Extract data", "./logos/stackoverflow.jpg")
-        convert = Custom("Convert data", "./logos/xmltocsv.jpg")
+        extract = Custom("Extract Relevant Data", "./logos/extract.png")
         clean = Custom("Transform Data", "./logos/datatransform.jpg")
-        load = Custom("load data into snowflake", "./logos/dataset.jpg")
-        embeddings = Custom("Store embeddings", "./logos/openai.jpg")
+        load = Custom("Load Data into Bigquery", "./logos/gcpbigquery.png")
+        embeddings = Custom("Generate embeddings", "./logos/sbert.png")
+        grtexp = Custom("Great Expectations Analysis","./logos/greatexpectations.jpg")
 
-        airflow >> extract >> convert >> clean >> load >> embeddings
+        airflow >> extract >> clean >> load >> embeddings >> grtexp
 
     with Cluster("Streamlit"):
         signup = Custom("user sign up", "./logos/usersignup.jpg")
         signin = Custom("logged-in user", "./logos/user.jpg")
-        category = Custom("Select category and tags", "./logos/userselection.jpg")
+        category = Custom("Select a category", "./logos/userselection.jpg")
         #user input
-        user_input = Custom("User input", "./logos/userinput.jpg")
+        user_input = Custom("Ask a question", "./logos/userinput.jpg")
+
+        sbert_logo = Custom("Sbert Similarity Search", "./logos/sbert.png")
 
         with Cluster("OpenAI"):
-            openai_logo = Custom("", "./logos/openai.jpg")
-            similar_topics = Server("similarity search")
-            summary_display = Server("OpenAI summary")
-            langchain_answer = Server("Langchain generative QA")
+            openai_summary = Custom("Summarization", "./logos/openai.jpg")
+            openai_genqa = Custom("Generative Q/A", "./logos/openai.jpg")
+            openai_craft = Custom("Craft a Question", "./logos/openai.jpg")
+            openai_summary >> openai_genqa >> openai_craft
 
-            openai_logo >> similar_topics >> summary_display >> langchain_answer
-
-        streamlit >> signup >> signin >> category >> user_input >> openai_logo
+        streamlit >> signup >> signin >> category >> user_input >> sbert_logo >> openai_summary
 
     
 
     with Cluster("Streamlit Other Features"):
         stats = Custom("Stackoverflow stats", "./logos/stackoverflow.jpg")
-        expectations = Custom("Great Expectations", "./logos/greatexpectations.jpg")
         dashboard = Custom("User Dashboard", "./logos/userdashboard.jpg")
 
-        streamlit >> stats >> expectations >> dashboard
+        streamlit >> stats >> dashboard
 
 
 
@@ -67,15 +66,15 @@ with Diagram("Proposed Architecture for STACKAI", show=True):
 
         docker >> dockerize_airflow >> dockerize_fastapi >> dockerize_streamlit
 
-    streamlit<<Edge(label="Dockerise the whole app")<<docker>>terraform>>Edge(label="Deploy app on GCP")>>gcp
+    streamlit<<Edge(label="Dockerise the whole app")<<docker>>terraform>>Edge(label="Create GCP Infrastructure")>>gcp
 
-    with Cluster("Github Actions"):
-        push_project = Custom("Pytest", "./logos/pytest.jpg")
-        pytest_unit_test = Custom("Pylint", "./logos/pylint.jpg")
+    with Cluster("Pytest"):
+        push_project = Custom("Unit Testing", "./logos/unittesting.png")
+        pytest_unit_test = Custom("API Testing", "./logos/apitesting.png")
 
         github >> push_project >> pytest_unit_test
 
-    gcp << Edge(label="Continuous Integration and Deployment") >> github
+    gcp << Edge(label="Continuous Integration") >> github
 
-    streamlit<<Edge(label="Used to make API calls")>>fastapi
-    fastapi<<Edge(label="Connect with credentials")>>snowflake<<Edge(label="Perform Extract, Transform & Load")<<airflow
+    streamlit<<Edge(label="Used to make API calls")>>fastapi>>Edge(label="Access User Database")>>cloudsql
+    bigquery<<Edge(label="Data Source & Big Data Storage")>>Edge(label="Data Pipeline")>>airflow
