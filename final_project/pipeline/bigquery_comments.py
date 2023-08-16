24cf5990009c40fb4ed1f180cc69ce0162258102
Vedant-Deshpande <114270105+Vedant-Deshpande@users.noreply.github.com>
2023-08-16 13:09:07 -0400

    Update bigquery_comments.py

--- final_project/pipeline/bigquery_comments.py
+++ final_project/pipeline/bigquery_comments.py
@@ -2,28 +2,27 @@
 #inserts values into comments table (already created by terraform)
 from google.cloud import bigquery
 
-# Set up BigQuery client with explicit project ID
-bq_client = bigquery.Client(project='stackai-394819')
+def insert_data_into_comments_table(bq_client):
+    query = """
+        INSERT INTO StackAI.comments_cleaned (post_id, text, creation_date, score)
+        SELECT
+            c.post_id,
+            c.text,
+            c.creation_date,
+            c.score
+        FROM
+            `bigquery-public-data.stackoverflow.comments` c
+        JOIN
+            `stackai-394819.StackAI.posts_cleaned` p
+        ON 
+            c.post_id = p.question_id
+    """
+    job_config = bigquery.QueryJobConfig()
+    job_config.use_legacy_sql = False
+    
+    job = bq_client.query(query, job_config=job_config)
+    job.result()
 
-# Set up query to insert data into the table
-query = """
-    INSERT INTO StackAI.comments_cleaned (post_id, text, creation_date, score)
-    SELECT
-        c.post_id,
-        c.text,
-        c.creation_date,
-        c.score
-    FROM
-        `bigquery-public-data.stackoverflow.comments` c
-    JOIN
-        `stackai-394819.StackAI.posts_cleaned` p
-    ON 
-        c.post_id = p.question_id
-"""
-
-# Run query to insert data into the table
-job_config = bigquery.QueryJobConfig()
-job_config.use_legacy_sql = False
-
-job = bq_client.query(query, job_config=job_config)
-job.result()
+if __name__ == "__main__":
+    bq_client = bigquery.Client(project='stackai-394819')
+    insert_data_into_comments_table(bq_client)
